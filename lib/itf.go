31afa6bf59c44ce12697e56a3e8dadfc4546326d
y-du <42994541+y-du@users.noreply.github.com>
2024-06-28 10:15:27 +0200

    reduce api size

--- lib/itf.go
+++ lib/itf.go
@@ -7,33 +7,9 @@ import (
 )
 
 type Api interface {
-	baseApi
-	clientApi
-	userApi
-}
-
-type ClientApi interface {
-	baseApi
-	clientApi
-}
-
-type UserApi interface {
-	baseApi
-	userApi
-}
-
-type baseApi interface {
 	GetDevice(ctx context.Context, id string) (model.Device, error)
 	GetDevices(ctx context.Context, filter model.DevicesFilter) (map[string]model.Device, error)
 	DeleteDevice(ctx context.Context, id string) error
-	srv_info_lib.Api
-}
-
-type clientApi interface {
-	AddDevice(ctx context.Context, deviceBase model.DeviceBase) error
-	UpdateDevice(ctx context.Context, deviceBase model.DeviceBase) error
-}
-
-type userApi interface {
 	UpdateDeviceUserData(ctx context.Context, id string, userDataBase model.DeviceUserDataBase) error
+	srv_info_lib.Api
 }
