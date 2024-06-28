bd6d22c5902fd19cbf9c8268b223577640784762
y-du <42994541+y-du@users.noreply.github.com>
2024-06-28 09:20:29 +0200

    refactor and add client and user apis

--- lib/itf.go
+++ lib/itf.go
@@ -7,12 +7,33 @@ import (
 )
 
 type Api interface {
-	SetDevice(ctx context.Context, deviceBase model.DeviceBase) error
-	AddDevice(ctx context.Context, deviceBase model.DeviceBase) error
+	baseApi
+	clientApi
+	userApi
+}
+
+type ClientApi interface {
+	baseApi
+	clientApi
+}
+
+type UserApi interface {
+	baseApi
+	userApi
+}
+
+type baseApi interface {
 	GetDevice(ctx context.Context, id string) (model.Device, error)
 	GetDevices(ctx context.Context, filter model.DevicesFilter) (map[string]model.Device, error)
-	UpdateDevice(ctx context.Context, deviceBase model.DeviceBase) error
-	UpdateDeviceUserData(ctx context.Context, id string, userDataBase model.DeviceUserDataBase) error
 	DeleteDevice(ctx context.Context, id string) error
 	srv_info_lib.Api
+}
+
+type clientApi interface {
+	AddDevice(ctx context.Context, deviceBase model.DeviceBase) error
+	UpdateDevice(ctx context.Context, deviceBase model.DeviceBase) error
+}
+
+type userApi interface {
+	UpdateDeviceUserData(ctx context.Context, id string, userDataBase model.DeviceUserDataBase) error
 }
