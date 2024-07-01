1fec2ea265addc8ad5c77b1a2f0f23ba5f32e6ef
y-du <42994541+y-du@users.noreply.github.com>
2024-07-01 11:34:00 +0200

    init

--- include/storage_schema.sql
+++ include/storage_schema.sql
@@ -2,13 +2,13 @@ CREATE TABLE IF NOT EXISTS devices
 (
     id          TEXT NOT NULL,
     ref         TEXT NOT NULL,
-    name        TEXT,
-    state       TEXT,
+    name        TEXT DEFAULT '',
+    state       TEXT DEFAULT '',
     type        TEXT NOT NULL,
     created     TEXT NOT NULL,
-    updated     TEXT,
-    usr_name    TEXT,
-    usr_updated TEXT,
+    updated     TEXT DEFAULT '',
+    usr_name    TEXT DEFAULT '',
+    usr_updated TEXT DEFAULT '',
     PRIMARY KEY (id)
 );
 CREATE TABLE IF NOT EXISTS device_attributes
@@ -16,6 +16,6 @@ CREATE TABLE IF NOT EXISTS device_attributes
     dev_id   TEXT    NOT NULL,
     is_usr   INTEGER NOT NULL,
     key_name TEXT    NOT NULL,
-    value    TEXT,
+    value    TEXT DEFAULT '',
     FOREIGN KEY (dev_id) REFERENCES devices (id) ON DELETE CASCADE ON UPDATE RESTRICT
 );
