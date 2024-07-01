02efc99d53f598391646dc068606fa7af00ee9d6
y-du <42994541+y-du@users.noreply.github.com>
2024-07-01 08:55:04 +0200

    restructure

--- lib/model/device.go
+++ lib/model/device.go
@@ -8,18 +8,22 @@ type DeviceMethod = string
 
 type Device struct {
 	DeviceBase
-	Created  time.Time      `json:"created"`
-	Updated  time.Time      `json:"updated"`
 	UserData DeviceUserData `json:"user_data,omitempty"`
 }
 
 type DeviceBase struct {
-	ID  string `json:"id"`
-	Ref string `json:"ref"`
 	DeviceData
+	Created time.Time `json:"created"`
+	Updated time.Time `json:"updated"`
 }
 
 type DeviceData struct {
+	ID  string `json:"id"`
+	Ref string `json:"ref"`
+	DeviceDataBase
+}
+
+type DeviceDataBase struct {
 	Name       string            `json:"name"`
 	State      DeviceState       `json:"state"`
 	Type       string            `json:"type"`
@@ -42,9 +46,9 @@ type DeviceAttribute struct {
 }
 
 type DeviceMessage struct {
-	Method   DeviceMethod `json:"method"`
-	DeviceID string       `json:"device_id"`
-	Data     *DeviceData  `json:"data"`
+	Method   DeviceMethod    `json:"method"`
+	DeviceID string          `json:"device_id"`
+	Data     *DeviceDataBase `json:"data"`
 }
 
 type DevicesFilter struct {
